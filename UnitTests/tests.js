47bd606ade319fc17a7d1f5b96083c8ea3410294
nlpcsh <nikolay.petrov@abv.bg>
2018-03-13 18:31:53 +0200

    unit testing initial

---
+++ UnitTests/tests.js
@@ -0,0 +1,10 @@
+let chai = require('chai');
+let expect = chai.expect;
+
+let objects = require('../app/js/objects');
+
+describe('Testing', function(){
+    it('Test 1', function(){
+        expect(2).to.eq(2);
+    })
+})
