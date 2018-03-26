032e27ceeec8a6f2f1b47b81c7676cd456cf5c82
Nikolay Petrov <nikolay.petrov@abv.bg>
2018-03-27 00:13:51 +0300

    Added routers on the server

--- server/app.js
+++ server/app.js
@@ -1,12 +1,25 @@
+'use strict';
+
 let express = require('express');
+let bodyParser = requre('body-parser');
 let app = express();
 
 app.use(express.static('../app'))
 
-app.get('/', function (req, res) {
+app.use(bodyParser.json());
+
+require('./models');
+// routes // https://youtu.be/Da4DL5-9JOI?t=4201
+require('./routers')(app);
+
+app.get('/', function(req, res) {
     res.send('Hello World!');
 })
 
-app.listen(8000, function () {
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+})
+
+app.listen(8000, function() {
     console.log('Example app listening on port 8000!');
 });
---
+++ server/models/log-model.js
@@ -0,0 +1,3 @@
+'use strict';
+
+// https://youtu.be/Da4DL5-9JOI?t=5930
