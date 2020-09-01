584b1777cd3ee488767c832dbc38c3fbdb6ca544
Nikolay Petrov <nikolay.petrov@abv.bg>
2020-09-01 13:44:08 +0300

    added about and contact routs, tempates. Edited home and login templates

--- server/controllers/main-controller.js
+++ server/controllers/main-controller.js
@@ -4,6 +4,12 @@ module.exports = function(data) {
     return {
         getHome(req, res) {
             res.status(200).render('home');
+        },
+        getContacts(req, res) {
+            res.status(200).render('contacts');
+        },
+        getAbout(req, res) {
+            res.status(200).render('about');
         }
     }
 }
--- server/routers/main-router.js
+++ server/routers/main-router.js
@@ -7,4 +7,8 @@ const mainController = createMainController();
 
 router.get('/home', mainController.getHome);
 
+router.get('/about', mainController.getAbout);
+
+router.get('/contacts', mainController.getContacts);
+
 module.exports = app => app.use(router);
