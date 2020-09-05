0d3b571ae60de2472a025ddc2aea25dc4b1f3a02
Nikolay Petrov <nikolay.petrov@abv.bg>
2020-09-05 11:26:45 +0300

    redirect to login if unauthorized

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -5,7 +5,7 @@ module.exports = function() {
     return {
         getProfile(req, res) {
             if (!req.isAuthenticated()) {
-                res.status(401).redirect('/unauthorized');
+                res.status(401).redirect('/login');
             } else {
                 const user = req.user;
                 res.status(200).render('profile', user);
