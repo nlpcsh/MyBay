92e67c884ec1fda14e43cc0d8421a4ee4c2632c5
Nikolay Petrov <nikolay.petrov@abv.bg>
2020-09-05 11:25:27 +0300

    user model updated

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -5,8 +5,41 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        lowercase: true,
+        required: true,
+        trim: true,
+        index: true,
+        unique: true,
+        maxlength: 50,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    googleId: String,
+    firstName: {
+        type: String,
+        trim: true,
+        maxlength: 50
+    },
+    lastName: {
+        type: String,
+        maxlength: 50,
+        trim: true
+    },
+    info: {
+        type: String,
+        maxlength: 200
+    },
+    whishList: Array,
+    image: {
+        data: Buffer, 
+        contentType: String 
+    },
+    orders: Array,
+    created: {
+        type : Date,
+        default: Date.now
+    }
 });
 
 userSchema.plugin(passportLocalMongoose);
