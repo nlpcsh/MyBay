99a93d952053b043502d1042185fce61392d43f3
Nikolay Petrov <nikolay.petrov@abv.bg>
2020-09-16 14:16:55 +0300

    added items model and sample items list in the DB

---
+++ server/data/test-items.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = [
+    {
+        owner: '5f5349a9c838e63c108d4785',
+        name: 'Ocean Fisher',
+        model: 'Motor Fishing',
+        price: 15000,
+        image: 'img/boat1.jpg',
+        description: 'Very Fast',
+        available:  true
+    },
+    {
+        owner: '5f5347e7c838e63c108d4784',
+        name: 'Max Entertainer',
+        model: 'Motor Entertainer',
+        price: 25000,
+        image: 'img/boat2.jpg',
+        description: 'Very Fast and Entertaining',
+        available:  true
+    },
+    {
+        owner: '5f5349a9c838e63c108d4785',
+        name: 'Windmaker',
+        model: 'Catamaran',
+        price: 18000,
+        image: 'img/boat3.jpg',
+        description: 'Not so fast but provides a smooth ride',
+        available:  true
+    },
+    {
+        owner: '5f5347e7c838e63c108d4784',
+        name: 'Stable sailsman',
+        model: 'Trimaran',
+        price: 34000,
+        image: 'img/boat4.jpg',
+        description: 'Stability and comfort',
+        available:  true
+    },
+    {
+        owner: '5f5349a9c838e63c108d4785',
+        name: 'Captain Sparrow',
+        model: 'Flagman ship',
+        price: 280000,
+        image: 'img/boat5.jpg',
+        description: 'To sail in open sea!',
+        available:  true
+    },
+]
